065e49142b2501c1161d3f56323994eb034c2e97
EOConnell99 <eoconne6@charlotte.edu>
2025-10-29 21:53:44 -0400

    feat: controller for resources

--- api/controllers/sandwiches.py
+++ api/controllers/sandwiches.py
@@ -21,7 +21,7 @@ def read_one(db: Session, sandwich_id):
 
 def update(db: Session, sandwich_id, sandwich):
     db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
-    update_data = sandwich.dict(exclude_unset=True)
+    update_data = sandwich.model_dump(exclude_unset=True)
     db_sandwich.update(update_data, synchronize_session=False)
     db.commit()
     return db_sandwich.first()
